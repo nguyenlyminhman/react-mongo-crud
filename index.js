b812799b2e9bee044705169ea05a9d06bdc2d1f5
nguyenlyminhman <nguyenlyminhman@gmail.com>
2018-05-27 23:01:09 +0700

    remove react-create-class. add create-react-class. Add api router for project

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require ('express');
 const config =  require ('config');
+const apiRouter = require('./api/index');
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', './views')
 
-app.get('/',(req,res)=>{
-    res.send('ok man')
-})
+app.use(express.static('./public'));
+
+app.use('/', apiRouter);
+
 var port = config.get("server.port")
 app.listen(port, (req,res)=>{
     console.log('Server is listening on port', port )
-})
+});
--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-document.write('Ok man')
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import createClass from 'react-create-class';
+let React = require('react');
+let ReactDOM = require('react-dom');
+let createClass = require('create-react-class');
+
+var Note = createClass({
+    render(){
+        return(
+            <div>
+                <h1> {this.props.title} </h1>
+                <p>  {this.props.quantity} </p>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <Note title='Nodemon' quantity='10' />,
+    document.getElementById('root')
+);
